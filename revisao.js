bb97b575a950df31f94716c1fcef00457d3d6058
niluiz <nicoleduarteluna@hotmail.com>
2019-08-14 19:40:48 -0300

    Revisão - ts

---
+++ revisao.js
@@ -0,0 +1,5 @@
+var nota1 = 10;
+var nota2 = 9;
+var nota3 = 8;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média das três notas é igual a: " + media);
---
+++ revisao.ts
@@ -0,0 +1,9 @@
+let nota1: number = 10
+let nota2: number = 9
+let nota3: number = 8
+
+let media: number = (nota1 + nota2 + nota3) / 3
+
+console.log ("A média das três notas é igual a: " + media)
+
+
