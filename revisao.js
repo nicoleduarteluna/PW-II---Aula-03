aaf44b4a2a56d977b98c0d78f95df80ebaa29be1
niluiz <nicoleduarteluna@hotmail.com>
2019-08-14 21:45:01 -0300

    Alteração - revisão

--- revisao.js
+++ revisao.js
@@ -1,5 +1,9 @@
-var nota1 = 10;
-var nota2 = 9;
-var nota3 = 8;
-var media = (nota1 + nota2 + nota3) / 3;
-console.log("A média das três notas é igual a: " + media);
+var n1 = 2;
+var n2 = 6;
+var n3 = 10;
+// let media: number = (nota1 + nota2 + nota3) / 3
+var fMedia = function (nota1, nota2, nota3) {
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("A media das notas " + n1 + ", " + n2 + " e " + n3 + " \u00E9: " + fMedia(n1, n2, n3));
+// console.log ("A média das três notas é igual a: " + fMedia(n1, n2, n3))
--- revisao.ts
+++ revisao.ts
@@ -1,9 +1,15 @@
-let nota1: number = 10
-let nota2: number = 9
-let nota3: number = 8
+let n1: number = 2
+let n2: number = 6
+let n3: number = 10
 
-let media: number = (nota1 + nota2 + nota3) / 3
+// let media: number = (nota1 + nota2 + nota3) / 3
 
-console.log ("A média das três notas é igual a: " + media)
+let fMedia = function(nota1:number, nota2:number, nota3:number): number{
+    return (nota1 + nota2 + nota3) / 3
+}
+
+console.log(`A media das notas ${n1}, ${n2} e ${n3} é: ${fMedia(n1,n2,n3)}`)
+
+// console.log ("A média das três notas é igual a: " + fMedia(n1, n2, n3))
 
 
