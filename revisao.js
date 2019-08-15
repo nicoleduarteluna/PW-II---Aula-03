9447bd935af80a04036c553ee56a02e961782540
niluiz <nicoleduarteluna@hotmail.com>
2019-08-14 21:55:58 -0300

    Utilização de função seta

--- revisao.js
+++ revisao.js
@@ -2,8 +2,9 @@ var n1 = 2;
 var n2 = 6;
 var n3 = 10;
 // let media: number = (nota1 + nota2 + nota3) / 3
-var fMedia = function (nota1, nota2, nota3) {
-    return (nota1 + nota2 + nota3) / 3;
-};
-console.log("A media das notas " + n1 + ", " + n2 + " e " + n3 + " \u00E9: " + fMedia(n1, n2, n3));
+//let fMedia = function(nota1:number, nota2:number, nota3:number): number{
+//    return (nota1 + nota2 + nota3) / 3
+// }
+var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
+console.log("A media \u00E9: " + fMedia(n1, n2, n3));
 // console.log ("A média das três notas é igual a: " + fMedia(n1, n2, n3))
--- revisao.ts
+++ revisao.ts
@@ -4,11 +4,14 @@ let n3: number = 10
 
 // let media: number = (nota1 + nota2 + nota3) / 3
 
-let fMedia = function(nota1:number, nota2:number, nota3:number): number{
-    return (nota1 + nota2 + nota3) / 3
-}
+//let fMedia = function(nota1:number, nota2:number, nota3:number): number{
+//    return (nota1 + nota2 + nota3) / 3
+// }
 
-console.log(`A media das notas ${n1}, ${n2} e ${n3} é: ${fMedia(n1,n2,n3)}`)
+
+let fMedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3) /3
+
+console.log(`A media é: ${fMedia(n1,n2,n3)}`)
 
 // console.log ("A média das três notas é igual a: " + fMedia(n1, n2, n3))
 
