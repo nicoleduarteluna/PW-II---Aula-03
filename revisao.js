1164ce372ee49bc72d1e0f16662fed770658d78a
niluiz <nicoleduarteluna@hotmail.com>
2019-08-14 22:12:58 -0300

    Valor Padrão (Função)

--- revisao.js
+++ revisao.js
@@ -1,10 +1,13 @@
 var n1 = 2;
 var n2 = 6;
-var n3 = 10;
+// let n3: number = 10
 // let media: number = (nota1 + nota2 + nota3) / 3
 //let fMedia = function(nota1:number, nota2:number, nota3:number): number{
 //    return (nota1 + nota2 + nota3) / 3
 // }
-var fMedia = function (nota1, nota2, nota3) { return (nota1 + nota2 + nota3) / 3; };
-console.log("A media \u00E9: " + fMedia(n1, n2, n3));
+var fMedia = function (nota1, nota2, nota3) {
+    if (nota3 === void 0) { nota3 = 10; }
+    return (nota1 + nota2 + nota3) / 3;
+};
+console.log("A media \u00E9: " + fMedia(n1, n2));
 // console.log ("A média das três notas é igual a: " + fMedia(n1, n2, n3))
--- revisao.ts
+++ revisao.ts
@@ -1,6 +1,6 @@
 let n1: number = 2
 let n2: number = 6
-let n3: number = 10
+// let n3: number = 10
 
 // let media: number = (nota1 + nota2 + nota3) / 3
 
@@ -9,10 +9,8 @@ let n3: number = 10
 // }
 
 
-let fMedia = (nota1:number, nota2:number, nota3:number) => (nota1 + nota2 + nota3) /3
+let fMedia = (nota1:number, nota2:number, nota3:number=10 ) => (nota1 + nota2 + nota3) /3
 
-console.log(`A media é: ${fMedia(n1,n2,n3)}`)
+console.log(`A media é: ${fMedia(n1,n2)}`)
 
 // console.log ("A média das três notas é igual a: " + fMedia(n1, n2, n3))
-
-
